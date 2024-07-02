a9b7241d327eeddfea793728033ab9badbd225f8
sssajjad007 <sssajjad.76@gmail.com>
2024-07-02 17:37:02 +0330

    chore: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   extends: ['@react-native-community', 'prettier'],
   rules: {
     'prettier/prettier': ['error', prettierConfig],
+    'react-hooks/exhaustive-deps': 'off',
   },
   ignorePatterns: ['node_modules/', 'dist/', 'copied-src/'],
 };
